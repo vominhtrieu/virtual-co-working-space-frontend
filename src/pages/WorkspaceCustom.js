ae5fda77ee1d6629dbe9f530a184a2b4a384b2d8
ndmt1at21 <trminh36@gmail.com>
2021-12-17 19:15:11 +0700

    fix warning

--- src/pages/WorkspaceCustom.js
+++ src/pages/WorkspaceCustom.js
@@ -4,7 +4,6 @@ import Box from "../components/Models/Box";
 import Chair from "../components/Models/Chair";
 import { Canvas } from "@react-three/fiber";
 import Button from "../components/Button";
-import * as THREE from "three";
 import { useRef, useState } from "react";
 import { RightArrow } from "../assets/icons/RightArrow";
 import { LeftArrow } from "../assets/icons/LeftArrow";
