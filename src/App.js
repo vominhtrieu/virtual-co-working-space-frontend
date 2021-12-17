82888e1da7aacf411287952273c37ef801486f4d
thuy13705 <18120587@student.hcmus.edu.vn>
2021-12-17 17:30:27 +0700

    showlistedititem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import 'antd/dist/antd.css';
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Scene from "./components/Scene";
+import ShowListEditItem from './components/listItem/ShowListEditItem';
+import Chair from './components/Models/Chair';
 
 function App() {
+    const name="Chair";
+
+    const items = [Chair, Chair, Chair, Chair,Chair];
+
     return (
-        <div className="App">
+        <div className="App" >
+
             <Canvas
                 shadows={{ enabled: true, autoUpdate: true }}
                 camera={{ position: [0, 20, 10], rotation: [45, 0, 0] }}
@@ -12,6 +20,8 @@ function App() {
             >
                 <Scene />
             </Canvas>
+
+            <ShowListEditItem items={items} name={name}/>
         </div>
     );
 }
