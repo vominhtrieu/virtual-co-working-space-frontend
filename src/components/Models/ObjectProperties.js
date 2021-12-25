35b2b6f522f82688d2e627e9bc3ff29857e01b50
trieuvo <trieuvo@busmap.vn>
2021-12-25 21:14:35 +0700

    Minor fixes

--- src/components/Models/ObjectProperties.js
+++ src/components/Models/ObjectProperties.js
@@ -3,23 +3,22 @@ import Chair from "./Chair";
 import IndoorTree from "./IndoorTree";
 import ModernTable from "./ModernTable";
 import SofaChair from "./SofaChair";
-import YellowChair from "./YellowChair";
 import Hair from "./Hair";
 import Eye from "./Eye";
 import Keyboard from "./Keyboard";
 import CoffeeTable from "./CoffeeTable";
 
 const ObjectProperties = {
-  Chair: <Chair />,
-  IndoorTree: <IndoorTree />,
-  ModernTable: <ModernTable />,
-  SofaChair: <SofaChair />,
-  Hair1: <Hair hairStyle={1} />,
-  Hair2: <Hair hairStyle={2} />,
-  Eyes1: <Eye eyeStyle={1} />,
-  Eye2: <Eye eyeStyle={2} />,
-  Keyboard: <Keyboard />,
-  CoffeeTable: <CoffeeTable />,
+    Chair: <Chair />,
+    IndoorTree: <IndoorTree />,
+    ModernTable: <ModernTable />,
+    SofaChair: <SofaChair />,
+    Hair1: <Hair hairStyle={1} />,
+    Hair2: <Hair hairStyle={2} />,
+    Eyes1: <Eye eyeStyle={1} />,
+    Eye2: <Eye eyeStyle={2} />,
+    Keyboard: <Keyboard />,
+    CoffeeTable: <CoffeeTable />,
 };
 
 export default ObjectProperties;
