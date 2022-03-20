ffea45760c515cb7f503e447483373903e529e76
Ho Tien <hotien2107@gmail.com>
2022-03-20 14:13:52 +0700

    save data local storage

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -2,6 +2,7 @@
 import { Col, Row } from "antd";
 import loginImage from "../../../assets/images/login/login-image.png";
 import LoginForm from "../../../components/login/loginForm";
+import { saveDataLocal } from "../../../helpers/localStorage";
 import LoginProxy from "../../../services/proxy/auth/login";
 import { setAuthenticated, setUserInfo } from "../../../stores/auth-slice";
 import { ProxyStatusEnum } from "../../../types/http/proxy/ProxyStatus";
@@ -17,15 +18,18 @@ function Login() {
         console.log(res);
         if (res.status === ProxyStatusEnum.FAIL) {
           console.log("login fail");
+          // show toast login fail
         }
 
         if (res.status === ProxyStatusEnum.SUCCESS) {
           setAuthenticated(true);
           setUserInfo(res?.data.userInfo);
+          saveDataLocal("user_id", res.data.userInfo.id);
         }
       })
       .catch((err) => {
         console.log(err);
+        // show toast login fail
       })
       .finally(() => {});
   };
