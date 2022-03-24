e132612b9e755442dcce1784a39f15876d9d15f6
Ho Tien <hotien2107@gmail.com>
2022-03-24 22:13:00 +0700

    done create side bar

--- src/components/layouts/sidebar/index.tsx
+++ src/components/layouts/sidebar/index.tsx
@@ -1,11 +1,23 @@
+import { BsFillChatFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+import { RiSettings4Fill } from "react-icons/ri";
+
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <div className='sidebar__logo'>A</div>
-      
+      <div className='sidebar__logo'>VW</div>
+
       <div className='sidebar__menu'>
         <ul className='sidebar__items'>
-          <li className='sidebar__item'></li>
+          <li className='sidebar__item'>
+            <BsFillChatFill className='sidebar__icon' />
+          </li>
+          <li className='sidebar__item'>
+            <FaUserAlt className='sidebar__icon' />
+          </li>
+          <li className='sidebar__item'>
+            <RiSettings4Fill className='sidebar__icon' />
+          </li>
         </ul>
       </div>
     </div>
