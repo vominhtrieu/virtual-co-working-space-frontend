a611dea6f8b7cb6b7dd216aed540ba6ea552ca59
Ho Tien <hotien2107@gmail.com>
2022-03-24 20:04:44 +0700

    create frame sidebar

---
+++ src/components/layouts/sidebar/index.tsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__logo'>A</div>
+      
+      <div className='sidebar__menu'>
+        <ul className='sidebar__items'>
+          <li className='sidebar__item'></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
