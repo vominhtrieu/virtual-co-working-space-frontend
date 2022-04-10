1b779ba193fb4301eea18445bd2b26f0c41baafb
Tien <hotien2107@gmail.com>
2022-04-10 17:42:19 +0700

    character custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import IconLanguages from "./components/icon-lang";
-import Sidebar from "./components/layouts/sidebar";
-import CharacterContext from "./context/CharacterContext";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-import CharacterCustom from "./pages/CharacterCustom";
-import Home from "./pages/home/Home";
-import NotFound from "./pages/notFound";
-import Workspace from "./pages/officeDetail/Workspace";
-import Active from "./pages/auth/active";
-import "./scss/main.scss";
-import { CharacterInterface } from "./types/character";
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import IconLanguages from './components/icon-lang'
+import Sidebar from './components/layouts/sidebar'
+import CharacterContext from './context/CharacterContext'
+import Active from './pages/auth/active'
+import Login from './pages/auth/login'
+import Register from './pages/auth/register'
+import CharacterCustom from './pages/characterCustom/intex'
+import NotFound from './pages/notFound'
+import Workspace from './pages/officeDetail/Workspace'
+import './scss/main.scss'
+import { CharacterInterface } from './types/character'
 
 function App() {
   const [character, setCharacter] = useState<CharacterInterface>({
     hairStyle: 1,
     eyeStyle: 1,
-  });
+  })
 
-  const { isAuthenticated } = useSelector((state: any) => state.auth);
+  const { isAuthenticated } = useSelector((state: any) => state.auth)
 
   return (
     <CharacterContext.Provider
@@ -35,28 +39,39 @@ function App() {
     >
       <ToastContainer />
       <IconLanguages />
-      <div className='App'>
+      <div className="App">
         <Router>
           {isAuthenticated ? <Sidebar /> : null}
           {isAuthenticated ? (
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/character' element={<CharacterCustom />} />
-              <Route path='/office/:id' element={<Workspace />} />
-              <Route path='*' element={<NotFound />} />
+              <Route path="/" element={<Navigate to="/character" replace />} />
+              <Route path="/character" element={<CharacterCustom />} />
+              <Route path="/office/:id" element={<Workspace />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           ) : (
             <Routes>
-              <Route path='/auth/login' element={<Login />} />
-              <Route path='/auth/register' element={<Register />} />
-              <Route path='/auth/activate/:token' element={<Active />} />
-              <Route path='*' element={<NotFound />} />
+              <Route path="/auth/login" element={<Login />} />
+              <Route path="/auth/register" element={<Register />} />
+              <Route path="/auth/activate/:token" element={<Active />} />
+              <Route path="*" element={<NotFound />} />
+
+              {/* redirect */}
+              <Route path="/" element={<Navigate to="/auth/login" replace />} />
+              <Route
+                path="/character"
+                element={<Navigate to="/auth/login" replace />}
+              />
+              <Route
+                path="/office/:id"
+                element={<Navigate to="/auth/login" replace />}
+              />
             </Routes>
           )}
         </Router>
       </div>
     </CharacterContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/layouts/sidebar/index.tsx
+++ src/components/layouts/sidebar/index.tsx
@@ -1,144 +1,160 @@
-import { useState } from "react";
-import { BsFillChatFill } from "react-icons/bs";
-import { FaUserAlt } from "react-icons/fa";
-import { MdMeetingRoom } from "react-icons/md";
-import { RiLogoutBoxRFill, RiSettings4Fill } from "react-icons/ri";
-import { useNavigate } from "react-router-dom";
-import { removeAllDataLocal } from "../../../helpers/localStorage";
-import { toastError, toastSuccess } from "../../../helpers/toast";
-import LogoutProxy from "../../../services/proxy/auth/logout";
-import { useAppDispatch, useAppSelector } from "../../../stores";
-import { setAuthenticated, setUserInfo } from "../../../stores/auth-slice";
-import { setOpen, sidebarSelectors } from "../../../stores/sidebar-slice";
-import { ProxyStatusEnum } from "../../../types/http/proxy/ProxyStatus";
-import SidebarChat from "./chat";
-import Offices from "./offices";
-import SidebarSettings from "./settings";
-import SidebarUser from "./userInfo";
+import { useState } from 'react'
+import { BsFillChatFill } from 'react-icons/bs'
+import { FaUserAlt } from 'react-icons/fa'
+import { MdMeetingRoom } from 'react-icons/md'
+import {
+  RiLogoutBoxRFill,
+  RiSettings4Fill,
+  RiUserSettingsFill,
+} from 'react-icons/ri'
+import { useNavigate } from 'react-router-dom'
+import { removeAllDataLocal } from '../../../helpers/localStorage'
+import { toastError, toastSuccess } from '../../../helpers/toast'
+import LogoutProxy from '../../../services/proxy/auth/logout'
+import { useAppDispatch, useAppSelector } from '../../../stores'
+import { setAuthenticated, setUserInfo } from '../../../stores/auth-slice'
+import { setOpen, sidebarSelectors } from '../../../stores/sidebar-slice'
+import { ProxyStatusEnum } from '../../../types/http/proxy/ProxyStatus'
+import SidebarChat from './chat'
+import Offices from './offices'
+import SidebarSettings from './settings'
+import SidebarUser from './userInfo'
 
 const Sidebar = () => {
-  const dispatch = useAppDispatch();
-  const [sidebarOpen, setSidebarOpen] = useState("");
-  const currSidebar = useAppSelector(sidebarSelectors.getOpen);
-  const navigate = useNavigate();
+  const dispatch = useAppDispatch()
+  const [sidebarOpen, setSidebarOpen] = useState('')
+  const currSidebar = useAppSelector(sidebarSelectors.getOpen)
+  const navigate = useNavigate()
 
   const handleLogout = () => {
     LogoutProxy()
       .then((res) => {
         if (res.status === ProxyStatusEnum.FAIL) {
-          toastError(res.message ?? "Logout fail");
-          return;
+          toastError(res.message ?? 'Logout fail')
+          return
         }
 
         if (res.status === ProxyStatusEnum.SUCCESS) {
-          toastSuccess("Logout success");
-          dispatch(setAuthenticated(false));
-          dispatch(setUserInfo({}));
-          removeAllDataLocal();
-          navigate("/auth/login");
-          return;
+          toastSuccess('Logout success')
+          dispatch(setAuthenticated(false))
+          dispatch(setUserInfo({}))
+          removeAllDataLocal()
+          navigate('/auth/login')
+          return
         }
       })
       .catch((err) => {
-        toastError(err.message ?? "Logout fail");
-      });
-  };
+        toastError(err.message ?? 'Logout fail')
+      })
+  }
 
   return (
-    <div className='sidebar-container'>
-      <div className='sidebar'>
-        <div className='sidebar__logo'>VW</div>
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <div className="sidebar__logo">VW</div>
 
-        <div className='sidebar__menu'>
-          <ul className='sidebar__items'>
+        <div className="sidebar__menu">
+          <ul className="sidebar__items">
             <li
               className={
-                "sidebar__item" + (currSidebar === "chat" ? " active" : "")
+                'sidebar__item' + (currSidebar === 'character' ? ' active' : '')
               }
-              onClick={() =>
-                setSidebarOpen((curr) => {
-                  if (curr === "chat") {
-                    dispatch(setOpen(""));
-                    return "";
-                  }
-                  dispatch(setOpen("chat"));
-                  return "chat";
-                })
-              }
+              onClick={() => {
+                navigate('/character')
+                setSidebarOpen('character')
+                dispatch(setOpen('character'))
+              }}
             >
-              <BsFillChatFill className='sidebar__icon' />
+              <RiUserSettingsFill className="sidebar__icon" />
             </li>
             <li
               className={
-                "sidebar__item" + (currSidebar === "office" ? " active" : "")
+                'sidebar__item' + (currSidebar === 'chat' ? ' active' : '')
               }
               onClick={() =>
                 setSidebarOpen((curr) => {
-                  if (curr === "office") {
-                    dispatch(setOpen(""));
-                    return "";
+                  if (curr === 'chat') {
+                    dispatch(setOpen(''))
+                    return ''
                   }
-                  dispatch(setOpen("office"));
-                  return "office";
+                  dispatch(setOpen('chat'))
+                  return 'chat'
                 })
               }
             >
-              <MdMeetingRoom className='sidebar__icon' />
+              <BsFillChatFill className="sidebar__icon" />
+            </li>
+            <li
+              className={
+                'sidebar__item' + (currSidebar === 'office' ? ' active' : '')
+              }
+              onClick={() =>
+                setSidebarOpen((curr) => {
+                  if (curr === 'office') {
+                    dispatch(setOpen(''))
+                    return ''
+                  }
+                  dispatch(setOpen('office'))
+                  return 'office'
+                })
+              }
+            >
+              <MdMeetingRoom className="sidebar__icon" />
             </li>
           </ul>
         </div>
 
-        <div className='sidebar__menu'>
-          <ul className='sidebar__items'>
+        <div className="sidebar__menu">
+          <ul className="sidebar__items">
             <li
               className={
-                "sidebar__item" + (currSidebar === "settings" ? " active" : "")
+                'sidebar__item' + (currSidebar === 'settings' ? ' active' : '')
               }
               onClick={() =>
                 setSidebarOpen((curr) => {
-                  if (curr === "settings") {
-                    dispatch(setOpen(""));
-                    return "";
+                  if (curr === 'settings') {
+                    dispatch(setOpen(''))
+                    return ''
                   }
-                  dispatch(setOpen("settings"));
-                  return "settings";
+                  dispatch(setOpen('settings'))
+                  return 'settings'
                 })
               }
             >
-              <RiSettings4Fill className='sidebar__icon' />
+              <RiSettings4Fill className="sidebar__icon" />
             </li>
 
             <li
               className={
-                "sidebar__item" + (currSidebar === "user" ? " active" : "")
+                'sidebar__item' + (currSidebar === 'user' ? ' active' : '')
               }
               onClick={() =>
                 setSidebarOpen((curr) => {
-                  if (curr === "user") {
-                    dispatch(setOpen(""));
-                    return "";
+                  if (curr === 'user') {
+                    dispatch(setOpen(''))
+                    return ''
                   }
-                  dispatch(setOpen("user"));
-                  return "user";
+                  dispatch(setOpen('user'))
+                  return 'user'
                 })
               }
             >
-              <FaUserAlt className='sidebar__icon' />
+              <FaUserAlt className="sidebar__icon" />
             </li>
 
-            <li className={"sidebar__item"} onClick={handleLogout}>
-              <RiLogoutBoxRFill className='sidebar__icon' />
+            <li className={'sidebar__item'} onClick={handleLogout}>
+              <RiLogoutBoxRFill className="sidebar__icon" />
             </li>
           </ul>
         </div>
       </div>
 
-      {sidebarOpen === "settings" ? <SidebarSettings /> : null}
-      {sidebarOpen === "user" ? <SidebarUser /> : null}
-      {sidebarOpen === "office" ? <Offices /> : null}
-      {sidebarOpen === "chat" ? <SidebarChat /> : null}
+      {sidebarOpen === 'settings' ? <SidebarSettings /> : null}
+      {sidebarOpen === 'user' ? <SidebarUser /> : null}
+      {sidebarOpen === 'office' ? <Offices /> : null}
+      {sidebarOpen === 'chat' ? <SidebarChat /> : null}
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
