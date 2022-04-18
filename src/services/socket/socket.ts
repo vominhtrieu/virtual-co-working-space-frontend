392cb61031dc7fe8fc3a7cd365816042f0bdc9b6
hoangtrung-0722 <hoangtrung.220720@gmail.com>
2022-04-18 20:29:19 +0700

    get token for socket connection

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,11 +1,12 @@
 import io from "socket.io-client"
+import { getDataLocal } from "../../helpers/localStorage";
 
 const ENDPOINT = "https://api.vispace.tech";
 
 const socket = io(ENDPOINT, {
     transports: ["websocket"],
     auth: (cb) => {
-        cb({ accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEzLCJpYXQiOjE2NDk1ODA5OTgsImV4cCI6MTY1MTIzMjM3OTA5NSwiaXNzIjoidmlydHVhbHNwYWNlLmNvbSJ9.hSjqpgQ2Qdv0Bect4HHL39Ixncld9ggZ3IZj26ESPyo"})
+        cb({ accessToken: getDataLocal("token")})
     }
 });
 
