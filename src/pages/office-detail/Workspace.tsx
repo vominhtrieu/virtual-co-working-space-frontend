bd44180f5f7c0e03c23678a28544190197a22c42
unknown <hotien2107@gmail.com>
2022-05-25 18:50:39 +0700

    merge ui

--- src/pages/office-detail/Workspace.tsx
+++ src/pages/office-detail/Workspace.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import CharacterForm from "../../components/character-form";
@@ -24,24 +24,24 @@ export type positionType = {
 };
 
 const Workspace = () => {
-    const [conversationId, setConversationId] = useState<number>(0);
-    const [isOwner, setIsOwner] = useState(false);
-    const {open} = useSelector((state: any) => state.sidebar);
-    const [isShowDetailForm, setIsShowDetailForm] = useState(false);
-    const [isShowChatBox, setIsShowChatBox] = useState(false);
-    const [objectList, setObjectList] = useState<OfficeItem[]>([]);
-    const [selectedKey, setSelectedKey] = useState(null);
-    const [selectedObject, setSelectedObject] = useState<any>(null);
-    const [objectActionVisible, setObjectActionVisible] = useState(false);
-    const [object3dClickPos, setObjectionClickPos] = useState<positionType>({
-        x: 0,
-        y: 0,
-    });
-    const [isCustomizing, setIsCustomizing] = useState(false);
-    const [characterGesture, setCharacterGesture] = useState({idx: -1});
-    const [characterEmoji, setCharacterEmoji] = useState({idx: -1});
+  const [conversationId, setConversationId] = useState<number>(0);
+  const [isOwner, setIsOwner] = useState(false);
+  const { open } = useSelector((state: any) => state.sidebar);
+  const [isShowDetailForm, setIsShowDetailForm] = useState(false);
+  const [isShowChatBox, setIsShowChatBox] = useState(false);
+  const [objectList, setObjectList] = useState<OfficeItem[]>([]);
+  const [selectedKey, setSelectedKey] = useState(null);
+  const [selectedObject, setSelectedObject] = useState<any>(null);
+  const [objectActionVisible, setObjectActionVisible] = useState(false);
+  const [object3dClickPos, setObjectionClickPos] = useState<positionType>({
+    x: 0,
+    y: 0,
+  });
+  const [isCustomizing, setIsCustomizing] = useState(false);
+  const [characterGesture, setCharacterGesture] = useState({ idx: -1 });
+  const [characterEmoji, setCharacterEmoji] = useState({ idx: -1 });
 
-    const [action, setAction] = useState<
+  const [action, setAction] = useState<
     | "action"
     | "character"
     | "config"
@@ -50,205 +50,239 @@ const Workspace = () => {
     | "chatBox"
     | "setting"
     | ""
-    >("");
+  >("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const location = useLocation();
+  const location = useLocation();
 
-    const locationState: any = location.state;
-    const officeId = locationState["officeId"];
+  const locationState: any = location.state;
+  const officeId = locationState["officeId"];
 
-    const userInfo = useAppSelector(userSelectors.getUserInfo);
+  const userInfo = useAppSelector(userSelectors.getUserInfo);
 
-    const socket = useAppSelector(socketSelector.getSocket);
+  const socket = useAppSelector(socketSelector.getSocket);
 
-    const handleObject3dDragged = useCallback((position, rotation) => {
-        socket.emit("office_item:move", {
-            id: selectedKey,
-            transform: {
-                xRotation: rotation.x,
-                yRotation: rotation.y,
-                zRotation: rotation.z,
-                xPosition: position.x,
-                yPosition: position.y,
-                zPosition: position.z
-            }
-        })
-    }, [selectedKey, socket]);
-    
-    const handleObject3dRotated = () => {
-        socket.emit("office_item:move", {
-            id: selectedKey,
-            transform: {
-                xRotation: selectedObject.rotation.x,
-                yRotation: selectedObject.rotation.y,
-                zRotation: selectedObject.rotation.z,
-                xPosition: selectedObject.position.x,
-                yPosition: selectedObject.position.y,
-                zPosition: selectedObject.position.z
-            }
-        })
-    }
+  const handleObject3dDragged = useCallback(
+    (position, rotation) => {
+      socket.emit("office_item:move", {
+        id: selectedKey,
+        transform: {
+          xRotation: rotation.x,
+          yRotation: rotation.y,
+          zRotation: rotation.z,
+          xPosition: position.x,
+          yPosition: position.y,
+          zPosition: position.z,
+        },
+      });
+    },
+    [selectedKey, socket]
+  );
 
-    const handleButtonRotateLeftClick = () => {
-        selectedObject.rotation.y += Math.PI / 2;
-        handleObject3dRotated();
+  const handleObject3dRotated = () => {
+    socket.emit("office_item:move", {
+      id: selectedKey,
+      transform: {
+        xRotation: selectedObject.rotation.x,
+        yRotation: selectedObject.rotation.y,
+        zRotation: selectedObject.rotation.z,
+        xPosition: selectedObject.position.x,
+        yPosition: selectedObject.position.y,
+        zPosition: selectedObject.position.z,
+      },
+    });
+  };
+
+  const handleButtonRotateLeftClick = () => {
+    selectedObject.rotation.y += Math.PI / 2;
+    handleObject3dRotated();
+  };
+
+  const handleButtonRotateRightClick = () => {
+    selectedObject.rotation.y -= Math.PI / 2;
+    handleObject3dRotated();
+  };
+
+  const handleButtonDeleteClick = () => {
+    const idx = objectList.findIndex((obj) => obj.id === selectedKey);
+    const newObjectList = [...objectList];
+    newObjectList.splice(idx, 1);
+    // navigate("/");
+    setObjectList([...newObjectList]);
+    setSelectedObject(null);
+    setSelectedKey(null);
+    setObjectActionVisible(false);
+    socket.emit("office_item:delete", selectedKey);
+  };
+
+  useEffect(() => {
+    socket.on("office_item:created", (message) => {
+      console.log(message);
+      setObjectList((objectList) => [...objectList, message]);
+    });
+
+    socket.on("office_item:moved", (message) => {
+      const { id, transform } = message;
+      const idx = objectList.findIndex((obj) => obj.id === id);
+      const newObjectList = [...objectList];
+      // change newObjectList[idx] transform
+      newObjectList[idx].transform = {
+        xPosition: transform.xPosition,
+        yPosition: transform.yPosition,
+        zPosition: transform.zPosition,
+        xRotation: transform.xRotation,
+        yRotation: transform.yRotation,
+        zRotation: transform.zRotation,
+      };
+      setObjectList([...newObjectList]);
+    });
+
+    socket.on("office_item:deleted", (message) => {
+      const idx = objectList.findIndex((obj) => obj.id === message);
+      const newObjectList = [...objectList];
+      newObjectList.splice(idx, 1);
+      setObjectList([...newObjectList]);
+    });
+
+    return () => {
+      socket.removeListener("office_item:created");
+      socket.removeListener("office_item:moved");
     };
+  }, [socket, objectList]);
 
-    const handleButtonRotateRightClick = () => {
-        selectedObject.rotation.y -= Math.PI / 2;
-        handleObject3dRotated();
-    };
+  useEffect(() => {
+    console.log("join office ", officeId);
+    socket.emit("office_member:join", {
+      officeId: officeId,
+    });
+  }, [officeId, socket]);
 
-    const handleButtonDeleteClick = () => {
-        const idx = objectList.findIndex((obj) => obj.id === selectedKey);
-        const newObjectList = [...objectList];
-        newObjectList.splice(idx, 1);
-        // navigate("/");
-        setObjectList([...newObjectList]);
-        setSelectedObject(null);
-        setSelectedKey(null);
-        setObjectActionVisible(false);
-        socket.emit("office_item:delete", selectedKey);
-    };
+  const handleItemInBottomMenuClick = (item: Item) => {
+    // setObjectList((objectList) => [
+    //     ...objectList,
+    //     item,
+    // ]);
+    socket.emit("office_item:create", {
+      itemId: item.id,
+      officeId: officeId,
+      xPosition: 0,
+      yPosition: 1,
+      zPosition: 0,
+      xRotation: 0,
+      yRotation: 0,
+      zRotation: 0,
+    });
+  };
 
-    useEffect(() => {
-        socket.on("office_item:created", (message) => {
-            console.log(message);
-            setObjectList((objectList) => [
-                ...objectList,
-                message
-            ])
-        })
-
-        socket.on("office_item:moved", (message) => {
-            const {id, transform} = message
-            const idx = objectList.findIndex((obj) => obj.id === id);
-            const newObjectList = [...objectList];
-            // change newObjectList[idx] transform
-            newObjectList[idx].transform = {
-                xPosition: transform.xPosition,
-                yPosition: transform.yPosition,
-                zPosition: transform.zPosition,
-                xRotation: transform.xRotation,
-                yRotation: transform.yRotation,
-                zRotation: transform.zRotation
-            }
-            setObjectList([...newObjectList]);
-        })
-
-        socket.on("office_item:deleted", (message) => {
-            const idx = objectList.findIndex((obj) => obj.id === message);
-            const newObjectList = [...objectList];
-            newObjectList.splice(idx, 1);
-            setObjectList([...newObjectList]);
-        })
-
-        return () => {
-            socket.removeListener("office_item:created");
-            socket.removeListener("office_item:moved");
+  useEffect(() => {
+    OfficeDetailProxy({ id: officeId })
+      .then((res) => {
+        if (res.status === ProxyStatusEnum.FAIL) {
+          return;
         }
-    }, [socket, objectList]);
 
-    useEffect(() => {
-        console.log("join office ", officeId);
-        socket.emit("office_member:join", {
-            officeId: officeId
-        })
-    }, [officeId, socket]);
+        if (res.status === ProxyStatusEnum.SUCCESS) {
+          setIsOwner(res?.data?.office?.createdBy?.id === userInfo.id);
+          if (res?.data?.office?.conversations.length > 0) {
+            setConversationId(res?.data?.office?.conversations[0]?.id);
+          }
+          if (res?.data?.office?.officeItems.length > 0) {
+            setObjectList(res.data.office.officeItems);
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [officeId, userInfo.id]);
 
-    const handleItemInBottomMenuClick = (item: Item) => {
-        // setObjectList((objectList) => [
-        //     ...objectList,
-        //     item,
-        // ]);
-        socket.emit("office_item:create", {
-            itemId: item.id,
-            officeId: officeId,
-            xPosition: 0,
-            yPosition: 1,
-            zPosition: 0,
-            xRotation: 0,
-            yRotation: 0,
-            zRotation: 0,
-        })
-    };
+  useEffect(() => {
+    console.log("Object list: ", objectList);
+  }, [objectList]);
 
-    useEffect(() => {
-        OfficeDetailProxy({id: officeId})
-            .then((res) => {
-                if (res.status === ProxyStatusEnum.FAIL) {
-                    return;
-                }
+  return (
+    <>
+      {!isCustomizing && <CallingBar />}
+      <OfficeCanvas
+        setObjectionClickPos={setObjectionClickPos}
+        characterGesture={characterGesture}
+        characterEmoji={characterEmoji}
+        isCustomizing={isCustomizing}
+        objectActionVisible={objectActionVisible}
+        objectList={objectList}
+        selectedObject={selectedObject}
+        selectedKey={selectedKey}
+        setObjectActionVisible={setObjectActionVisible}
+        setSelectedKey={setSelectedKey}
+        setSelectedObject={setSelectedObject}
+        handleObject3dDragged={handleObject3dDragged}
+      />
+      <OfficeInterface
+        open={open}
+        conversationId={conversationId}
+        isShowChatBox={isShowChatBox}
+        isCustomizing={isCustomizing}
+        objectActionVisible={objectActionVisible}
+        handleButtonDeleteClick={handleButtonDeleteClick}
+        handleButtonRotateLeftClick={handleButtonRotateLeftClick}
+        handleButtonRotateRightClick={handleButtonRotateRightClick}
+        handleItemInBottomMenuClick={handleItemInBottomMenuClick}
+        object3dClickPos={object3dClickPos}
+        isOwner={isOwner}
+        setCharacterEmoji={setCharacterEmoji}
+        setCharacterGesture={setCharacterGesture}
+        setIsCustomizing={setIsCustomizing}
+        setIsShowDetailForm={setIsShowDetailForm}
+        setIsShowChatBox={setIsShowChatBox}
+        action={action}
+        setAction={setAction}
+      />
+      {isShowDetailForm ? (
+        <OfficeDetailForm
+          onClose={() => {
+            setIsShowDetailForm(false);
+          }}
+          id={officeId}
+          isOwner={isOwner}
+        />
+      ) : null}
 
-                if (res.status === ProxyStatusEnum.SUCCESS) {
-                    setIsOwner(res?.data?.office?.createdBy?.id === userInfo.id);
-                    if (res?.data?.office?.conversations.length > 0) {
-                        setConversationId(res?.data?.office?.conversations[0]?.id);
-                    }
-                    if (res?.data?.office?.officeItems.length > 0) {
-                        setObjectList(res.data.office.officeItems);
-                    }
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, [officeId, userInfo.id]);
+      {action === "chatList" && (
+        <ChatList
+          onClose={() => setAction("")}
+          id={+officeId}
+          onSelectConversation={handleSelectConversation}
+        />
+      )}
 
-    useEffect(() => {
-      console.log("Object list: ", objectList)
-    }, [objectList])
+      {action === "chatBox" && (
+        <ChatBox
+          conversationId={conversationId}
+          onBack={() => setAction("chatList")}
+          onClose={() => setAction("")}
+          submitMessage={handleSubmitMessage}
+        />
+      )}
 
-    return (
-        <>
-            {!isCustomizing && <CallingBar/>}
-            <OfficeCanvas
-                setObjectionClickPos={setObjectionClickPos}
-                characterGesture={characterGesture}
-                characterEmoji={characterEmoji}
-                isCustomizing={isCustomizing}
-                objectActionVisible={objectActionVisible}
-                objectList={objectList}
-                selectedObject={selectedObject}
-                selectedKey={selectedKey}
-                setObjectActionVisible={setObjectActionVisible}
-                setSelectedKey={setSelectedKey}
-                setSelectedObject={setSelectedObject}
-                handleObject3dDragged={handleObject3dDragged}
-            />
-            <OfficeInterface
-                open={open}
-                conversationId={conversationId}
-                isShowChatBox={isShowChatBox}
-                isCustomizing={isCustomizing}
-                objectActionVisible={objectActionVisible}
-                handleButtonDeleteClick={handleButtonDeleteClick}
-                handleButtonRotateLeftClick={handleButtonRotateLeftClick}
-                handleButtonRotateRightClick={handleButtonRotateRightClick}
-                handleItemInBottomMenuClick={handleItemInBottomMenuClick}
-                object3dClickPos={object3dClickPos}
-                isOwner={isOwner}
-                setCharacterEmoji={setCharacterEmoji}
-                setCharacterGesture={setCharacterGesture}
-                setIsCustomizing={setIsCustomizing}
-                setIsShowDetailForm={setIsShowDetailForm}
-                setIsShowChatBox={setIsShowChatBox}
-                action={action}
-                setAction={setAction}
-            />
-            {isShowDetailForm ? (
-                <OfficeDetailForm
-                    onClose={() => {
-                        setIsShowDetailForm(false);
-                    }}
-                    id={officeId}
-                    isOwner={isOwner}
-                />
-            ) : null}
-        </>
-    );
+      {action === "member" && (
+        <MemberList onClose={() => setAction("")} id={+officeId} />
+      )}
+
+      {action === "character" && (
+        <CharacterForm onClose={() => setAction("")} />
+      )}
+
+      {action === "action" && (
+        <InteractionMenu
+          onGestureClick={(value: number) =>
+            setCharacterGesture({ idx: value })
+          }
+          onEmojiClick={(value: number) => setCharacterEmoji({ idx: value })}
+        />
+      )}
+    </>
+  );
 };
 
 export default Workspace;
