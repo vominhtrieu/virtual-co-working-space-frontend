01630d0faabca7fbc5fd127825553b083db5ea6e
hoangtrung-0722 <hoangtrung.220720@gmail.com>
2022-05-31 14:36:42 +0700

    minor fixes

--- src/pages/office-detail/Workspace.tsx
+++ src/pages/office-detail/Workspace.tsx
@@ -337,7 +337,7 @@ const Workspace = () => {
         <InteractionMenu
           onGestureClick={handleGestureClick}
           onEmojiClick={handleEmojiClick}
-O        />
+        />
       )}
     </>
   );
