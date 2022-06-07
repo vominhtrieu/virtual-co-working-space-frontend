eb97a00d8494895d4d48c8c5ec30b922cd9bd1b0
vominhtrieu <minhtrieuvo600@gmail.com>
2022-06-07 21:50:32 +0700

    Minor fix

--- src/pages/office-detail/calling/CallingBar.tsx
+++ src/pages/office-detail/calling/CallingBar.tsx
@@ -1,146 +1,129 @@
 import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
 } from "react";
 import Peer from "peerjs";
 import "./_styles.scss";
-import { useParams } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../stores";
+import {useParams} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../stores";
 // import { connect } from "../../../stores/socket-slice";
-import { socketSelector } from "../../../stores/socket-slice";
-import { FaCamera, FaMicrophone } from "react-icons/fa";
-import { v4 } from "uuid";
+import {socketSelector} from "../../../stores/socket-slice";
+import {FaCamera, FaMicrophone} from "react-icons/fa";
+import {v4} from "uuid";
 
-export default function CallingBar() {
-  const videoContainer = useRef<any>();
-  const myVideo = useRef<any>();
-  const [myStream, setMyStream] = useState<MediaStream | null>(null);
-  const params = useParams();
-  const socket = useAppSelector(socketSelector.getSocket);
-  const [cameraEnabled, setCameraEnabled] = useState(true);
-  const [microphoneEnabled, setMicrophoneEnabled] = useState(true);
-  console.log(socket.connected);
-  const myPeer = useMemo(
-    () =>
-      new Peer(v4(), {
-        host: process.env.REACT_APP_PEER_SERVER_HOST + "",
-        port: +(process.env.REACT_APP_PEER_SERVER_PORT + ""),
-        path: "/peer",  
-      }),
-    []
-  );
-  const addVideoStream = useCallback(
-    (video: HTMLVideoElement, stream: MediaStream) => {
-      video.srcObject = stream;
-      const videoWrapper = document.createElement("div");
-      videoWrapper.className = "video-container";
-      videoWrapper.append(video);
+export default function CallingBar({
+                                       myStream,
+                                       setMyStream,
+                                       setOtherStreams,
+                                       userInfo,
+                                   }: any) {
+    const videoContainer = useRef<any>();
+    const myVideo = useRef<any>();
+    const params = useParams();
+    const socket = useAppSelector(socketSelector.getSocket);
+    const [cameraEnabled, setCameraEnabled] = useState(true);
+    const [microphoneEnabled, setMicrophoneEnabled] = useState(true);
 
-      video.addEventListener("loadedmetadata", () => {
-        video.play();
-      });
+    const myPeer = useMemo(
+        () => {
+            const p = new Peer(userInfo.id + "", {
+                host: process.env.REACT_APP_PEER_SERVER_HOST + "",
+                port: +(process.env.REACT_APP_PEER_SERVER_PORT + ""),
+                path: "/peer",
+            });
+            return p;
+        }, [userInfo.id]
+    );
+    const addVideoStream = useCallback(
+        (video: HTMLVideoElement, stream: MediaStream) => {
+            video.srcObject = stream;
+            const videoWrapper = document.createElement("div");
+            videoWrapper.className = "video-container";
+            videoWrapper.append(video);
 
-      if (videoContainer.current) {
-        videoContainer.current.append(videoWrapper);
-      }
-    },
-    []
-  );
+            video.addEventListener("loadedmetadata", () => {
+                video.play();
+            });
 
-  useEffect(() => {
-    myPeer.on("open", (id) => {
-      socket.emit("calling:join", {
-        officeId: params.id,
-        peerId: id,
-      });
-    });
-
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then((stream) => {
-        setMyStream(stream);
-        const streamMap = {};
-        myPeer.on("call", (call) => {
-          call.answer(stream);
-          const video = document.createElement("video");
-          call.on("stream", (userVideoStream) => {
-            if (streamMap[userVideoStream.id]) {
-              return;
+            if (videoContainer.current) {
+                videoContainer.current.append(videoWrapper);
             }
-            streamMap[userVideoStream.id] = true;
-            addVideoStream(video, userVideoStream);
-          });
+        },
+        []
+    );
+
+    useEffect(() => {
+        myPeer.on("open", (id) => {
+            socket.emit("calling:join", {
+                officeId: params.id,
+                peerId: id,
+            });
         });
-        if (myVideo.current) {
-          myVideo.current.srcObject = stream;
-        }
-      });
-  }, [myPeer, params.id]);
 
-  useEffect(() => {
-    if (!myStream || !myPeer) {
-      return;
-    }
-    const streamMap = {};
-    socket.on("calling:join", ({ userId, peerId }) => {
-      setTimeout(() => {
-        const call = myPeer.call(peerId, myStream);
-        const video = document.createElement("video");
+        navigator.mediaDevices
+            .getUserMedia({
+                video: {
+                    width: 360,
+                    height: 360,
+                },
+                audio: true,
+            })
+            .then((stream: any) => {
+                // stream.userId = userInfo.id;
+                setMyStream(stream);
+                const streamMap = {};
+                myPeer.on("call", (call) => {
+                    call.answer(stream);
+                    call.on("stream", (userVideoStream: any) => {
+                        if (streamMap[userVideoStream.id]) {
+                            return;
+                        }
+                        streamMap[userVideoStream.id] = true;
+                        setOtherStreams((streams) => {
+                            const temp = {...streams};
+                            temp[userVideoStream.userId] = userVideoStream;
+                            return temp;
+                        })
+                    });
+                });
+                if (myVideo.current) {
+                    myVideo.current.srcObject = stream;
+                }
+            });
+    }, [myPeer, params.id]);
 
-        call.on("stream", (userVideoStream) => {
-          if (streamMap[userVideoStream.id]) {
+    useEffect(() => {
+        if (!myStream || !myPeer) {
             return;
-          }
-          streamMap[userVideoStream.id] = true;
-          addVideoStream(video, userVideoStream);
-        });
-        call.on("close", () => {
-          video.remove();
-        });
-      }, 1000);
-    });
-    return () => {
-      socket.removeListener("calling:join");
-    };
-  }, [addVideoStream, myPeer, myStream]);
+        }
+        const streamMap = {};
+        socket.on("calling:join", ({userId, peerId}) => {
+            setTimeout(() => {
+                const call = myPeer.call(peerId, myStream);
 
-  return (
-    <div ref={videoContainer} className="calling-bar-container">
-      {myStream && (
-        <div className="video-container">
-          <video ref={myVideo} autoPlay muted />
-          <div className="video-button-container">
-            <button
-              className={`video-button ${cameraEnabled ? "active" : ""}`}
-              onClick={() => {
-                const vidTrack = myStream?.getVideoTracks();
-                vidTrack.forEach((track) => (track.enabled = !cameraEnabled));
-                setCameraEnabled(!cameraEnabled);
-              }}
-            >
-              <FaCamera fontSize={20} />
-            </button>
-            <button
-              className={`video-button ${microphoneEnabled ? "active" : ""}`}
-              onClick={() => {
-                const vidTrack = myStream?.getAudioTracks();
-                vidTrack.forEach(
-                  (track) => (track.enabled = !microphoneEnabled)
-                );
-                setMicrophoneEnabled(!microphoneEnabled);
-              }}
-            >
-              <FaMicrophone fontSize={20} />
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+                call.on("stream", (userVideoStream: any) => {
+                    if (streamMap[userVideoStream.id]) {
+                        return;
+                    }
+                    streamMap[userVideoStream.id] = true;
+                    setOtherStreams((streams) => {
+                        const temp = {...streams};
+                        temp[userId] = userVideoStream;
+                        return temp;
+                    })
+                });
+                call.on("close", () => {
+                    // video.remove();
+                });
+            }, 1000);
+        });
+        return () => {
+            socket.removeListener("calling:join");
+        };
+    }, [addVideoStream, myPeer, myStream]);
+
+    return null;
 }
