fe21e90696c073c1d17b8dbff806a2a1dec7b6fa
hotien2107 <hotien2107@gmail.com>
2022-06-24 00:11:47 +0700

    update home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
+import { HiOutlineVideoCamera } from "react-icons/hi";
+import { MdChatBubbleOutline, MdOutlineScreenShare } from "react-icons/md";
+import { FaRegBuilding, FaRegHandScissors, FaRegUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-import Button from "../../components/UI/button";
-import imgsrc from "../../assets/images/Saly-10.png";
+import aboutImage from "../../assets/images/about-img1.jpg";
 import DarkLogo from "../../assets/images/DarkLogo.png";
-import NewButton from "../../components/UI/new-button";
-import { FaSignInAlt } from "react-icons/fa";
+import imgsrc from "../../assets/images/Saly-10.png";
+import Button from "../../components/UI/button";
+import hhvtImg from "../../assets/images/team/HHVT.jpg";
+import nhtImg from "../../assets/images/team/NHT.png";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -42,7 +46,140 @@ const Home = () => {
         </div>
       </div>
 
-      {/* <div className="features"></div> */}
+      <div className="home__block--white">
+        <div className="home__our-story">
+          <img src={aboutImage} alt="" className="our-story__image" />
+          <div className="our-story__content">
+            <div className="our-story__header">
+              Ý tưởng ban đầu
+            </div>
+            <div className="our-story__title">
+              Văn phòng tại nhà
+            </div>
+            <div className="our-story__text">
+              Những năm gần đây, do ảnh hưởng của dịch Covid-19, việc sử dụng các ứng dụng làm việc, học trực tuyến trở thành lựa chọn hàng đầu cho các doanh nghiệp và trường học trong bối cảnh giãn cách. Bắt kịp xu thế đó, nhóm nãy ra ý tưởng làm nền tảng mô phỏng văn phòng 3D cho phép xây dựng một không gian học tập và làm việc ảo cho riêng người dùng. Mọi người có thể trò chuyện, tương tác với nhau để tránh nhàm chán khi làm việc, học tập trực tuyến.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="home__block--white">
+        <div className="home__services">
+          <div className="services__header">
+            Tính năng
+          </div>
+
+          <div className="services__list">
+            <div className="services__item">
+              <div className="services__icon-box">
+                <FaRegUser className="services__icon" />
+              </div>
+
+              <div className="services__title">Tạo nhân vật</div>
+              <div className="services__content">
+                Người dùng khi tham gia vào hệ thống sẽ có một nhân vật riêng và có thể tuỳ chỉnh nhân vật theo sở thích của bản thân.
+              </div>
+            </div>
+            <div className="services__item">
+              <div className="services__icon-box">
+                <FaRegBuilding className="services__icon" />
+              </div>
+
+              <div className="services__title">Tạo văn phòng</div>
+              <div className="services__content">
+                Người dùng có thể tạo văn phòng và thoả sức sáng tạo, trang trí văn phòng bằng các đồ dùng có sẵn.
+              </div>
+            </div>
+            <div className="services__item">
+              <div className="services__icon-box">
+                <FaRegHandScissors className="services__icon" />
+              </div>
+
+              <div className="services__title">Team building</div>
+              <div className="services__content">
+                Các thành viên có thể chơi các trò chơi với nhau để giải trí, giảm căng thẳng sau những giờ làm việc và tăng tính đoàn kết.
+              </div>
+            </div>
+            <div className="services__item">
+              <div className="services__icon-box">
+                <HiOutlineVideoCamera className="services__icon" />
+              </div>
+
+              <div className="services__title">Video call</div>
+              <div className="services__content">
+                Các thành viên khi vào phòng có thể video call với nhau giúp họ cảm thấy gần gũi hơn dù cách xa địa lý.
+              </div>
+            </div>
+            <div className="services__item">
+              <div className="services__icon-box">
+                <MdChatBubbleOutline className="services__icon" />
+              </div>
+
+              <div className="services__title">Tạo kênh trò chuyện</div>
+              <div className="services__content">
+                Có thể tạo ra nhiều kênh trò chuyện khác nhau để phù hợp với từng nhóm công việc.
+              </div>
+            </div>
+            <div className="services__item">
+              <div className="services__icon-box">
+                <MdOutlineScreenShare className="services__icon" />
+              </div>
+
+              <div className="services__title">Chia sẻ màn hình</div>
+              <div className="services__content">
+                Chúng ta có thể trình chiếu màn hình để thuyết trình trong các buổi họp.
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="home__block--white">
+        <div className="home__team">
+          <div className="team__header">
+            Thành viên
+          </div>
+
+          <div className="team__list">
+            <a href="https://www.facebook.com/profile.php?id=100005642647089" target="_blank" rel="noreferrer">
+              <div className="team__item">
+                <img src="https://ca.slack-edge.com/T03ANC0U03Y-U03ANC2SP1U-4924160679fc-512" alt="" className="team__item-avt" />
+                <div className="team__item-name">Nguyễn Lương Phương Thuỷ</div>
+                <div className="team__item-student-id">18120587</div>
+              </div>
+            </a>
+            <a href="https://www.facebook.com/tien.hohoang.7" target="_blank" rel="noreferrer">
+              <div className="team__item">
+                <img src={hhvtImg} alt="" className="team__item-avt" />
+                <div className="team__item-name">Hồ Hoàng Việt Tiến</div>
+                <div className="team__item-student-id">18120589</div>
+              </div>
+            </a>
+            <a href="https://www.facebook.com/tricua2000" target="_blank" rel="noreferrer">
+              <div className="team__item">
+                <img src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t1.6435-9/35817922_2082528495336145_534863839422840832_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=2TgyK1Qw_E8AX8hRhdw&_nc_ht=scontent.fsgn13-2.fna&oh=00_AT_4GVyHldTYGyxZcI42DmKJJxVKOzFab-jZdRveMz9QUA&oe=62D899B1" alt="" className="team__item-avt" />
+                <div className="team__item-name">Nguyễn Đức Minh Trí </div>
+                <div className="team__item-student-id">18120612</div>
+              </div>
+            </a>
+            <a href="https://www.facebook.com/trieu.vm" target="_blank" rel="noreferrer">
+              <div className="team__item">
+                <img src="https://ca.slack-edge.com/T03ANC0U03Y-U03AV1SJE90-a6e100f4f72d-512" alt="" className="team__item-avt" />
+                <div className="team__item-name">Võ Minh Triều</div>
+                <div className="team__item-student-id">18120615</div>
+              </div>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100005642647089" target="_blank" rel="noreferrer">
+              <div className="team__item">
+                <img src={nhtImg} alt="" className="team__item-avt" />
+                <div className="team__item-name">Nguyễn Hoàng Trung</div>
+                <div className="team__item-student-id">18120623</div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
